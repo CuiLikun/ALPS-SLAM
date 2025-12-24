7dc5f67f814e55b3e177947ca40aa9e300e5781b
CuiLikun <2649026548@qq.com>
2025-12-24 22:12:07 +0800

    继续解决ros消息不匹配的问题

--- local_planner/src/pathFollower.cpp
+++ local_planner/src/pathFollower.cpp
@@ -12,7 +12,7 @@
 #include <std_msgs/Float32.h>
 #include <nav_msgs/Path.h>
 #include <nav_msgs/Odometry.h>
-#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/Joy.h>
@@ -59,6 +59,7 @@ bool noRotAtGoal = true;
 bool autonomyMode = false;
 double autonomySpeed = 1.0;
 double joyToSpeedDelay = 2.0;
+string cmdVelTopic = "/cmd_vel";
 
 float joySpeed = 0;
 float joySpeedRaw = 0;
@@ -212,6 +213,7 @@ int main(int argc, char** argv)
   nhPrivate.getParam("autonomyMode", autonomyMode);
   nhPrivate.getParam("autonomySpeed", autonomySpeed);
   nhPrivate.getParam("joyToSpeedDelay", joyToSpeedDelay);
+  nhPrivate.getParam("cmdVelTopic", cmdVelTopic);
 
   ros::Subscriber subOdom = nh.subscribe<nav_msgs::Odometry> ("/state_estimation", 5, odomHandler);
 
@@ -223,9 +225,8 @@ int main(int argc, char** argv)
 
   ros::Subscriber subStop = nh.subscribe<std_msgs::Int8> ("/stop", 5, stopHandler);
 
-  ros::Publisher pubSpeed = nh.advertise<geometry_msgs::TwistStamped> ("/cmd_vel", 5);
-  geometry_msgs::TwistStamped cmd_vel;
-  cmd_vel.header.frame_id = "vehicle";
+  ros::Publisher pubSpeed = nh.advertise<geometry_msgs::Twist> (cmdVelTopic, 5);
+  geometry_msgs::Twist cmd_vel;
 
   if (autonomyMode) {
     joySpeed = autonomySpeed / maxSpeed;
@@ -331,10 +332,9 @@ int main(int argc, char** argv)
 
       pubSkipCount--;
       if (pubSkipCount < 0) {
-        cmd_vel.header.stamp = ros::Time().fromSec(odomTime);
-        if (fabs(vehicleSpeed) <= maxAccel / 100.0) cmd_vel.twist.linear.x = 0;
-        else cmd_vel.twist.linear.x = vehicleSpeed;
-        cmd_vel.twist.angular.z = vehicleYawRate;
+        if (fabs(vehicleSpeed) <= maxAccel / 100.0) cmd_vel.linear.x = 0;
+        else cmd_vel.linear.x = vehicleSpeed;
+        cmd_vel.angular.z = vehicleYawRate;
         pubSpeed.publish(cmd_vel);
 
         pubSkipCount = pubSkipNum;
